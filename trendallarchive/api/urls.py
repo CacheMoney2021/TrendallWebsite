5cbbf5afe6430a86c669aeb3b584307c8158adab
Ma Jecille Dizon <18491889@students.ltu.edu.au>
2021-09-30 22:03:43 +1000

    master

---
+++ trendallarchive/api/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+#from .views import GetTheVase, main
+from . import views
+
+
+urlpatterns = [
+    #path('', main),
+    #path('getthevase/', views.GetTheVase.as_view(), name="getthevase"),
+    #path('searchresults/', views.FilterVases.as_view(), name="searchresults"),
+]
