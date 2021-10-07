739c30c538726392fa7daa4f638063ad8c861424
Ma Jecille Dizon <18491889@students.ltu.edu.au>
2021-10-07 23:17:18 +1100

    master

--- trendallarchive/frontend/src/components/Vase.js
+++ trendallarchive/frontend/src/components/Vase.js
@@ -2,36 +2,16 @@ import React,{useEffect,useState} from 'react';
 import axios from 'axios';
 import styled from 'styled-components';
 
-const VaseTitle = styled.div`
-font-family: 'Roboto', sans-serif;
-font-size: 40px;
-font-weight: 400;
-line-height:2.5em;
-color: #3F342C;
-`
-
-const AttributeTitle = styled.div`
-    font-family: 'Roboto', sans-serif;
-    font-size: 17px;
-    font-weight: 300;
-    color: #3F342C;
-`
-const Attribute = styled(AttributeTitle)`
-    font-size: 14px;
-    width: 60%;
-    margin-bottom: 0px;
-`
 
 
 const FormDiv = styled.div`
     width: 100%;
     height: 100%;
     right: 0px;
-    padding: 35px;
+    padding:4%;
     display: absolute;
-    margin-top: 70px;
 `
-//takes vaseID parameter from the URL to pass into API below
+//takes vaseRef parameter from the URL to pass into API below
 const urlParams = new URLSearchParams(window.location.search);
 const vaseRef = urlParams.get('vaseRef');
 console.log(vaseRef);
@@ -52,7 +32,7 @@ const Vase=()=>{
         Provenance:''
     })
     useEffect(()=>{
-        axios.get(`http://127.0.0.1:8000/api/getthevase/?vaseRef=${vaseRef}`)//get the selected vase using the vaseID passed through the URL
+        axios.get(`http://127.0.0.1:8000/api/getthevase/?vaseRef=${vaseRef}`)//get the selected vase using the vaseRef passed through the URL
             .then(res=>{
                 console.log({vaseRef})
                 console.log('Response from main API: ',res) //printing the response to the console
@@ -64,24 +44,47 @@ const Vase=()=>{
             })
     },[])
 
-//return the vase into that was retrieved in the function
+//return and display the vase information that was retrieved in the function
     return(
         <>
 
         
         <FormDiv>
-           <VaseTitle>{Data.VaseRef}, {Data.Fabric}</VaseTitle> 
-           <Attribute><p>VASE NUMBER: {Data.VaseRef}</p></Attribute> 
-           <Attribute><p>Collection: {Data.Collection}</p></Attribute>
-           <Attribute><p>Publications: {Data.Publications}</p></Attribute>
-           <Attribute><p>Fabric: {Data.Fabric}</p></Attribute>
-            <Attribute><p>Technique: {Data.Technique}</p></Attribute>
-            <Attribute><p>Description: {Data.Subject}</p></Attribute>
-            <Attribute><p>Artist: {Data.ArtistName}</p></Attribute>
-            <Attribute><p>Shape: {Data.Shape}</p></Attribute>
-            <Attribute><p>Provenance: {Data.Provenance}</p></Attribute>
-            <Attribute><p>Height: {Data.Height} cm</p></Attribute>
-            <Attribute><p>Diameter: {Data.Diameter}</p></Attribute>
+            <div className = "vaseDiv">
+                <h2>{Data.VaseRef}, {Data.Fabric}, {Data.Collection}</h2>
+            <table>
+                <tr>
+                    <th>Vase Number</th> <td>{Data.VaseRef}</td> 
+                </tr>
+                <tr>
+                    <th>Collection</th> <td>{Data.Collection}</td> 
+                </tr>
+                <tr>
+                    <th>Fabric</th> <td>{Data.Fabric}</td> 
+                </tr>
+                <tr>
+                    <th>Technique</th> <td>{Data.Technique}</td> 
+                </tr>
+                <tr>
+                    <th>Description</th> <td>{Data.Subject}</td> 
+                </tr>
+                <tr>
+                    <th>Shape</th> <td>{Data.Shape}</td> 
+                </tr>
+                <tr>
+                    <th>Provenance</th> <td>{Data.Provenance}</td> 
+                </tr>
+                <tr>
+                    <th>Publications</th>  <td>{Data.Publications}</td> 
+                </tr>
+                    <th>Height</th><td>{Data.Height} cm</td>
+                <tr>
+                    <th>Diameter</th><td>{Data.Diameter} cm</td>
+                </tr>
+            </table>
+            </div>
+
+
         </FormDiv>
         </>
     )
--- trendallarchive/frontend/src/components/VaseGallery.js
+++ trendallarchive/frontend/src/components/VaseGallery.js
@@ -3,6 +3,7 @@ import {useEffect,useState} from 'react';
 import AliceCarousel from "react-alice-carousel";
 import "react-alice-carousel/lib/alice-carousel.css";
 import styled from 'styled-components';
+import axios from 'axios';
 import "./page_elements/VaseGallery.css";
 import BackBtn from '../components/buttons/BackBtn';
 import BannerVaseImage from '../components/images/basicsearchimage.png';
@@ -25,8 +26,46 @@ const FormDiv = styled.div`
     padding: 45px;
 `
 
-const Gallery = () => {
-  /*
+const VaseImg = (props) => {
+  return <img src={props.name} width="500px" height="auto"/>; 
+}
+const urlParams = new URLSearchParams(window.location.search);
+const vaseRef = urlParams.get('vaseRef');
+console.log(vaseRef);
+
+
+const getImg = (vaseRef)=>{
+  const [Data,setData]=useState({
+      plateRef:''
+  })
+  useEffect(()=>{
+    axios.get(`http://127.0.0.1:8000/api/getplate/?vase=${vaseRef}`)//get the selected vase using the vaseID passed through the URL
+        .then(res=>{
+            console.log('Response from main API: ',res) //printing the response to the console
+            let plateData=res.data[0]; //add data to vaseData, then assign into each variable
+            setData({plateRef:plateData.plateRef}) 
+        })
+        .catch(err=>{
+            console.log(err);
+        })
+      console.log(vaseRef)
+      console.log(plateRef)
+  },[])
+  return(
+    <>
+      {Data.plateRef}
+    </>
+  )
+}
+
+const images = [
+  //<img src={`https://trendallplates.blob.core.windows.net/images/${plateRef}.png`}/>//plateRef is returning as undefined
+  <img src={`https://trendallplates.blob.core.windows.net/images/image2.png`}/>,
+  <img src={`https://trendallplates.blob.core.windows.net/images/image2.png`}/>,
+  <img src={`https://trendallplates.blob.core.windows.net/images/image2.png`}/>
+];
+
+class Gallery extends React.Component {
   state = {
     galleryItems: images.map((i) => (
       <Wrapper>
@@ -34,52 +73,42 @@ const Gallery = () => {
       </Wrapper>
     ))
   };
-  */
+  responsive = {
+    0: { items: 1 },
+    1024: { items: 1 }
+  };
 
-  const urlParams = new URLSearchParams(window.location.search);
-    const vaseRef = urlParams.get('vaseRef');
-    console.log(vaseRef);
-  
-    const [Data,setData]=useState([])
-  
-    useEffect(()=>{
-      axios.get(`http://127.0.0.1:8000/api/getplate/?vase=${vaseRef}`)//get the selected vase using the vaseID passed through the URL
-          .then(res=>{
-              console.log('Response from Plate API: ',res.data[0]) //printing the response to the console
-              let plateData=res.data[0]; //add data to vaseData, then assign into each variable
-              setData({plateRef:plateData.vaseRef}) 
-          })
-          .catch(err=>{
-              console.log(err);
-          })
-        console.log(vaseRef)
-    },[])
-  
-  const images = [
-    <img src={`https://trendallplates.blob.core.windows.net/images/image2.png`}/>,
-    <img src={`https://trendallplates.blob.core.windows.net/images/image2.png`}/>,
-    <img src={`https://trendallplates.blob.core.windows.net/images/image2.png`}/>,
-    <img src={`https://trendallplates.blob.core.windows.net/images/image2.png`}/>,
-    <img src={`https://trendallplates.blob.core.windows.net/images/P-1-1.png`}/>    //plateRef is returning as undefined
-  ];
+  onSlideChange(e) {
+    console.debug("Item`s position during a change: ", e.item);
+    console.debug("Slide`s position during a change: ", e.slide);
+  }
 
+  onSlideChanged(e) {
+    console.debug("Item`s position after changes: ", e.item);
+    console.debug("Slide`s position after changes: ", e.slide);
+  }
+
+  render() {
     return (
         <FormDiv>
           {/*<BackBtn/>*/}
           <div className="img-gallery">
             <AliceCarousel
-              items={images}   //items={this.state.galleryItems}
+              items={this.state.galleryItems}
+              responsive={this.responsive}
               autoPlayInterval={2000}
               autoPlayDirection="ltr"
               autoPlay={false}
               fadeOutAnimation={true}
               playButtonEnabled={false}
               disableAutoPlayOnAction={true}
+              onSlideChange={this.onSlideChange}
+              onSlideChanged={this.onSlideChanged}
             />
           </div>
         </FormDiv>
     );
-  
+  }
 }
 
 export default Gallery;
